5ecc6fbd1e1c04bb4a22962f3ae18e673e9d0332
Mahmoud Ali <muddibr@gmail.com>
2016-04-14 11:28:11 -0300

    Initial commit

---
+++ src/ValidationTagHelperSample/Controllers/HomeController.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNet.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace ValidationTagHelperSample.Controllers
+{
+    public class User
+    {
+        [Required]
+        public string UserName1 { get; set; }
+
+        [Required]
+        public string UserName2 { get; set; }
+    }
+
+    public class HomeController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(User user)
+        {
+            if (ModelState.IsValid)
+                return RedirectToAction("Index");
+
+            return View(user);
+        }
+    }
+}
